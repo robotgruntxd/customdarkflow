4880042e5d819765df04ae9ac38eb6441f16996c
admin <asdasd@fasdfas.com>
2018-06-17 18:24:32 -0500

    rtsp protocol example with cv2

---
+++ ipcam.py
@@ -0,0 +1,7 @@
+import cv2
+
+vcap = cv2.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream--rtp-caching=100")
+while(1):
+    ret, frame = vcap.read()
+    cv2.imshow('VIDEO', frame)
+    cv2.waitKey(1)
